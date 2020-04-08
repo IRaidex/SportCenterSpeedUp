44c5e14e52c49b1026d13c818f7e60d0c900dbfe
Alejandro <alejandrosal1396@gmail.com>
2020-04-08 18:36:22 +0200

    Comenzando con el layaout

---
+++ resources/views/partes/cabecera.blade.php
@@ -0,0 +1,15 @@
+<header class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-8 ">
+            <div id="logo">
+                <a href="{{url('/')}}"><img src="{{asset('img/logo-rojo-recortado.png')}}" alt="logo" class=""></a>
+            </div>
+        </div>        
+        <div class="col-md-2 ">
+   
+        </div>
+    </div>
+
+
+
+</header>
