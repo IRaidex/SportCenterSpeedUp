c1063a7d44b1358b6f38d8427b85b3ef056aaca8
Alejandro <alejandrosal1396@gmail.com>
2020-04-09 19:42:33 +0200

    Cabecera y footer terminados

--- resources/views/partes/cabecera.blade.php
+++ resources/views/partes/cabecera.blade.php
@@ -1,15 +1,48 @@
 <header class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8 ">
+        <div class="col-md-5 col-12 text-center">
             <div id="logo">
-                <a href="{{url('/')}}"><img src="{{asset('img/logo-rojo-recortado.png')}}" alt="logo" class=""></a>
+                <a href="{{url('/')}}"><img src="{{asset('img/logo-rojo-recortado.png')}}" alt="logo"></a>
             </div>
         </div>        
-        <div class="col-md-2 ">
-   
+        <div class="col-md-5 d-md-none d-none d-lg-block">
+
+            <div class="headerText">
+                <h1>Servicio 24h<br>Sin permanencia</h1>
+            </div>
+
         </div>
     </div>
-
-
-
 </header>
+<nav class="navbar navbar-expand-md">
+    <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target=".opciones">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <ul class="collapse navbar-collapse navbar-nav opciones">
+        <li class="nav-item d-none d-md-block">
+            <a class="navbar-brand" href="{{url('/')}}">
+                <img class="logonav" src="{{asset('img/logo-rojo-recortado.png')}}" alt="logo">
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link color" href="{{url('categoria/Materiales')}}">Sobre Nosotros</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link color" href="{{url('categoria/Accesorios')}}">Servicios</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link color" href="{{url('formAgregarProducto')}}">Galeria</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link color" href="{{url('mostrar')}}">Torneos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link color" href="{{url('mostrar')}}">Contacto</a>
+        </li>
+    </ul>
+    <div class="form-inline">
+         <a href="" ><img class="svg mx-3" src="{{asset('img/login.svg')}}" alt="login"></a>  
+         <a href="" ><img class="svg mx-3" src="{{asset('img/logout.svg')}}" alt="logout"></a>
+         <a href="" ><img class="svg mx-3" src="{{asset('img/perfil.svg')}}" alt="perfil"></a>
+    </div>
+</nav>
--- resources/views/partes/footer.blade.php
+++ resources/views/partes/footer.blade.php
@@ -1,54 +1,40 @@
-<footer class="page-footer font-small blue-grey lighten-5 footer">
-    <div class="container-fluid text-center text-md-left">
-        <div class="row mt-3 dark-text mt-5">
-            <div class="col-md-3 col-lg-2 mx-auto mt-3">
-                <h6 class="text-uppercase font-weight-bold">Información</h6>
-                <hr class="mb-4 mt-0 d-inline-block w-50">
-                <p>
-                    <a class="text-warning" href="#!">¿Quienes Somos?</a>
-                </p>
-                <p>
-                    <a class="text-warning" href="#!">Terminos y condiciones</a>
-                </p>
-                <p>
-                    <a class="text-warning" href="#!">Trabaja con Nosotros</a>
-                </p>
-            </div>
-            <div class="col-md-3 col-lg-2 mx-auto mt-3">
-                <h6 class="text-uppercase font-weight-bold">Productos</h6>
-                <hr class="mb-4 mt-0 d-inline-block w-50">
-                <p>
-                    <a class="text-warning" href="#!">Ropa</a>
-                </p>
-                <p>
-                    <a class="text-warning" href="#!">Materiales</a>
-                </p>
-                <p>
-                    <a class="text-warning" href="#!">Maquinas</a>
-                </p>
-            </div>
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 mt-3">
-                <h6 class="text-uppercase font-weight-bold">Contacta con nosotros!</h6>
-                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 215px;">
-                <p>+34 685 21 22 56</p>
-                <p>964 55 41 78</p>
-                <p>contacto@gymfit.com</p>
-            </div>
+<footer class="container-fluid text-center">
+    <div class="row mt-5 blanco">
+        <div class="col-lg-4 col-12">
+            <h6 class="text-uppercase font-weight-bold mt-4">Información</h6>
+            <hr class="mb-4 mt-0 d-inline-block w-50">
+            <p class="foot">Terminos y condiciones</p>
+            <p class="foot">¿Quienes Somos?</p>
+            <p class="foot">Trabaja con Nosotros</p>
+        </div>
+        <div class="col-lg-4 col-12">
+            <h6 class="text-uppercase font-weight-bold mt-4">Productos</h6>
+            <hr class="mb-4 mt-0 d-inline-block w-50">
+            <p class="foot">Ropa</p>
+            <p class="foot">Materiales</p>
+            <p class="foot">Maquinas</p>
+        </div>
+        <div class="col-lg-4 col-12">
+            <h6 class="text-uppercase font-weight-bold mt-4">Contacta con nosotros!</h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 215px;">
+            <p class="foot">+34 670 35 75 62</p>
+            <p class="foot">961 33 14 99</p>
+            <p class="foot">contacto@speedup.com</p>
         </div>
     </div>
-    <div class="container-fluid fondoCopyEnlaces">
-        <div class="row py-3 d-flex">
-            <div class="col-md-6 col-lg-5 text-center mb-4">
-                <h6 class="mb-0 colorLetra">Siguenos en nuestras redes Sociales!</h6>
+    <div class="container-fluid mt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-3 col-12 text-center mt-3">
+                <h5 class="text-white">Siguenos en nuestras redes Sociales!</h5>
             </div>
-            <div class="col-md-6 col-lg-7 text-center">
-                <img class="redesSociales" src="{{asset('img/instagram.png')}}" alt="Instragram">
-                <img class="redesSociales" src="{{asset('img/twitter.png')}}" alt="Twitter">
-                <img class="redesSociales" src="{{asset('img/facebook.png')}}" alt="Facebook">
+            <div class="col-lg-3 col-12 text-center">
+                <img class="redesSociales m-1 svg" src="{{asset('img/instagram.png')}}" alt="Instragram">
+                <img class="redesSociales m-1 svg" src="{{asset('img/twitter.png')}}" alt="Twitter">
+                <img class="redesSociales m-1 svg" src="{{asset('img/facebook.png')}}" alt="Facebook">
             </div>
         </div>
-        <div class="footer-copyright text-center py-3 colorLetra">
-            © 2020 Copyright: GYMFIT
+        <div class="footer-copyright text-center mt-5 pb-3 text-white">
+            © 2020 Copyright: Speed Up
         </div>
     </div>
 </footer>
